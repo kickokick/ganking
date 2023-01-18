b28dbff2432098b8b807c4e8093be3fd40377c21
GoodMorning9000 <92028717+GoodMorning9000@users.noreply.github.com>
2023-01-17 19:37:59 -0500

    ok

--- Assets/scripts/keyhandler.cs
+++ Assets/scripts/keyhandler.cs
@@ -13,11 +13,6 @@ public class keyhandler : MonoBehaviour
     public Transform p1Camera;
     public Transform p2Camera;
 
-    float p1x;
-    float p1y;
-    float p2x;
-    float p2y;
-
     void Start ()
     {
 
@@ -25,61 +20,8 @@ public class keyhandler : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            p1y = 1.0f;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            p1y = -1.0f;
-        }
-        else
-        {
-            p1y = 0.0f;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            p1x = 1.0f;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            p1x = -1.0f;
-        }
-        else
-        {
-            p1x = 0.0f;
-        }
-
-        p1BodyRB.velocity = new Vector3(p1x, p1y, 0.0f);
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            p2y = 1.0f;
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            p2y = -1.0f;
-        }
-        else
-        {
-            p2y = 0.0f;
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            p2x = -1.0f;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            p2x = 1.0f;
-        }
-        else
-        {
-            p2x = 0.0f;
-        }
-
-        p2BodyRB.velocity = new Vector3(p2x, p2y, 0.0f);
+        p1BodyRB.velocity = new Vector3(Input.GetAxis("Player 1 Horizontal"), Input.GetAxis("Player 1 Vertical"), 0.0f);
+        p2BodyRB.velocity = new Vector3(Input.GetAxis("Player 2 Horizontal"), Input.GetAxis("Player 2 Vertical"), 0.0f);
 
         p2Camera.position = new Vector3(p2BodyT.position.x, p2BodyT.position.y, -10.0f);
         p1Camera.position = new Vector3(p1BodyT.position.x, p1BodyT.position.y, -10.0f);
