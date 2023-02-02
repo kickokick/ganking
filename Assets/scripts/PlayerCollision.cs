ad8c6682c187de41fc0b7030c0753f9f0538aef3
Harish kalari <kalariharish@gmail.com>
2023-02-01 19:59:46 -0500

    MAP AND ONTRIGGERRRRRRRRRRRRRRRR

--- Assets/scripts/PlayerCollision.cs
+++ Assets/scripts/PlayerCollision.cs
@@ -15,13 +15,13 @@ public class PlayerCollision : MonoBehaviour
         bxo = GetComponent<SpriteRenderer>();
     }
 
-    public void OnCollisionEnter2D(Collision2D coll){
+    public void OnTriggerEnter2D(Collider2D coll){
         if (coll.gameObject.tag == "box"){
             bxo.color = Color.red;
             playerText.color = new Color(0,0,0,255);
         }
     }
-    void OnCollisionExit2D(Collision2D coll){
+    void OnTriggerExit2D(Collider2D coll){
         if (coll.gameObject.tag == "box"){
             bxo.color = Color.white;
             playerText.color = new Color(0,0,0,0);
