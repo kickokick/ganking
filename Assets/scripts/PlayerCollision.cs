9e76ba06404916cb19ddad03a79559a41a3d954b
Harish kalari <kalariharish@gmail.com>
2023-02-27 18:39:21 -0500

    im tired

--- Assets/scripts/PlayerCollision.cs
+++ Assets/scripts/PlayerCollision.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UIElements;
 
 
 public class PlayerCollision : MonoBehaviour
@@ -22,10 +23,19 @@ public class PlayerCollision : MonoBehaviour
             playerText.color = new Color(0,0,0,255);
 
         }
+        else if (coll.gameObject.tag == "shop"){
+            playerText.color = new Color(0,0,0,255);
+            playerText.text = "Press \"E\"to interact";
+            if (Input.GetKeyDown("e")){
+            }
+        }
     }
     void OnTriggerExit2D(Collider2D coll){
         if (coll.gameObject.tag == "box"){
             player.color = Color.white;
+            playerText.color = new Color(0,0,0,0);
+        }
+        else if (coll.gameObject.tag == "shop"){
             playerText.color = new Color(0,0,0,0);
         }
     }
