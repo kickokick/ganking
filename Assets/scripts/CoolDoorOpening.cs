94353984ca7c9f4f2714aad4d74730acbfe2c428
Harish kalari <kalariharish@gmail.com>
2023-03-03 08:41:36 -0500

    karthik end your life

---
+++ Assets/scripts/CoolDoorOpening.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolDoorOpening : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void OnTriggerEnter2D(Collider2D coll){
+        anim.Play("CoolDoorBroke");
+    }
+    
+}
