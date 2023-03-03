84fe502fe7d20fbf53869a09f2ee498eba40574f
KarthikIndukuri <113394042+KarthikIndukuri@users.noreply.github.com>
2023-03-03 13:23:52 -0500

    mmmmmmmmmmmmmmmm

--- Assets/scripts/scene_changer.cs
+++ Assets/scripts/scene_changer.cs
@@ -5,8 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class scene_changer : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public void OnClick(string scene)
     {
         SceneManager.LoadScene(scene);
+=======
+    public void OnClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+>>>>>>> Stashed changes
     }
 }
